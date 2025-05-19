bb9535bb7a4b2c28767177cdfa80ac0150b4b3db
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-05-19 00:50:56 -0500

    V1.5 | Característica (animaciones): Añadir control de visibilidad de la entrada de búsqueda y actualizar el archivo CSS
    Añadir detectores de eventos para controlar la visibilidad de la entrada de búsqueda al pasar el cursor y hacer clic. Cambiar el nombre del archivo CSS de `index.css` a `main.css` y actualizar el enlace HTML según corresponda. Además, inicializar la entrada de búsqueda como oculta al cargar la página.

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,7 @@
 const upButton = document.querySelector('.up');
+const searchContainer = document.querySelector('.searchContainer');
+const inputSearch = document.querySelector('.inputSearch');
 
-// Función para mostrar/ocultar el botón basado en el scroll
 window.addEventListener('scroll', () => {
     if (window.scrollY > 800) { // Mostrar después de 300px de scroll
         upButton.style.display = 'flex';
@@ -9,6 +10,21 @@ window.addEventListener('scroll', () => {
     }
 });
 
+searchContainer.addEventListener('mouseover', () => {
+    inputSearch.style.display = 'flex';
+});
+
+inputSearch.addEventListener('click', () => {
+    inputSearch.style.display = 'flex';
+});
+
+document.addEventListener('click', (event) => {
+    if (!searchContainer.contains(event.target)) {
+        inputSearch.style.display = 'none';
+    }
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     upButton.style.display = 'none';
+    inputSearch.style.display = 'none';
 });
