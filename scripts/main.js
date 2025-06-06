4157885d965c6ae3c122776e8e67d9a957145125
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-06-06 15:30:05 -0500

    V1.9 | Refactorización (index.html, main.js, main.css): Se añaden funciones de navegación para iniciar sesión y registrarse, se elimina el archivo de animaciones, y se mejora la estructura y estilo del header.

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,27 @@
-const upButton = document.querySelector('.up');
-
-/* FUNCION MOSTRAR BOTON */
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) {
-        upButton.style.display = 'flex';
-    } else {
-        upButton.style.display = 'none';
+document.addEventListener("DOMContentLoaded", function() {
+    const upButton = document.querySelector('.up');
+    
+    /* FUNCTION SHOW BUTTON (UP) */
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            upButton.style.display = 'flex';
+        } else {
+            upButton.style.display = 'none';
+        }
+    });
+    
+    /* FUNCTION GO TO LOGIN PAGE */
+    function go_login() {
+        window.location.href = "templates/login.html";
+    }
+    
+    /* FUNCTION GO TO REGISTER PAGE */
+    function go_register() {
+        window.location.href = "templates/register.html";
     }
 });
 
-/* FUNCION MOSTRAR ENCABEZADO */
-function up_screen() {
-    window.location.href = "#topPage";
-}
+/* FUNCTION SHOW HEADER */
+    function up_screen() {
+        window.location.href = "#topPage";
+    }
