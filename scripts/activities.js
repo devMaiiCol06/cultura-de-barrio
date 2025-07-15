d259cd05d53c8c37ddbd011888ae7c362dbae7c7
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-07-14 20:24:50 -0500

    Cambios (actividades): Implementar el filtro desplegable de categorías y el filtrado de eventos
    - Añadir un menú desplegable para el filtrado de categorías en la página de actividades
    - Implementar el filtrado de eventos por categoría seleccionada
    - Actualizar el CSS para el estilo y las animaciones del menú desplegable
    - Refactorizar la lógica de conteo y renderizado de eventos
    - Añadir el manejo de la entrada de búsqueda (implementación incompleta)

--- scripts/activities.js
+++ scripts/activities.js
@@ -31,6 +31,18 @@ document.addEventListener("DOMContentLoaded", function () {
             const locations = data.LOCATIONS;
             const participants = data.PARTICIPANTS;
 
+            // Llama a la función para manejar el filtro de categorías
+            categoryFilter(
+                categories,
+                events,
+                category_event,
+                locations,
+                participants
+            );
+
+            // Llama a la función para contar y mostrar el total de eventos
+            countTotalEvents(events);
+
             // Llama a la función para renderizar eventos
             renderEventos(
                 events,
@@ -40,25 +52,30 @@ document.addEventListener("DOMContentLoaded", function () {
                 participants
             );
 
-            // Llama a la función para contar y mostrar el total de eventos
-            countTotalEvents(events);
+            // Llama a la función para filtrar eventos por nombre
+            filterEventsByName(
+                events,
+                participants,
+                locations,
+                categories,
+                category_event
+            );
         })
         // Maneja errores
         .catch((error) => console.error("Error:", error));
 
-    
     // Función para contar el total de eventos
     function countTotalEvents(events) {
-        return events.length;
-    }
-    // Llama a la función para contar el total de eventos
-    const totalEvents = countTotalEvents(events);
-    // Obtener el elemento HTML donde se mostrará el total de actividades
-    const activitiesCountElement = document.getElementById("activitiesCount");
-    // Verificar si el elemento existe
-    if (activitiesCountElement) {
-        // Establecer el contenido del elemento con el total de actividades
-        activitiesCountElement.innerHTML = `${totalEvents} actividades`;
+        // Guardar el total de eventos en una variable
+        const totalEvents = events.length;
+        // Obtener el elemento HTML donde se mostrará el total de actividades
+        const activitiesCountElement =
+            document.getElementById("activitiesCount");
+        // Verificar si el elemento existe o tiene contenido
+        if (activitiesCountElement) {
+            // Establecer el contenido del elemento con el total de actividades
+            activitiesCountElement.innerHTML = `${totalEvents} actividades`;
+        }
     }
 
     // Función para obtener las categorías de un evento
@@ -96,9 +113,10 @@ document.addEventListener("DOMContentLoaded", function () {
         locations,
         participants
     ) {
-
         // Obtener el contenedor de actividades
-        const activitiesContainer = document.querySelector(".activitiesContainer");
+        const activitiesContainer = document.querySelector(
+            ".activitiesContainer"
+        );
         // Limpiar el contenido anterior
         activitiesContainer.innerHTML = "";
         // Verificar si hay eventos disponibles
@@ -122,10 +140,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 participants
             );
             // Obtener la ubicación del evento mediante el llamado a la funcion
-            const locationName = obtenerUbicacionEvento(
-                event,
-                locations
-            )
+            const locationName = obtenerUbicacionEvento(event, locations);
 
             // Generar botón que enlaza a la vista detallada del evento usando su identificador único (evt_id)
             const buttonHTML = `<button onclick="window.location.href='activity.html?id=${event.evt_id}'">Ver más</button>`;
@@ -181,7 +196,9 @@ document.addEventListener("DOMContentLoaded", function () {
                             </div>
                             <div class="priceRegistered_activity">
                                 <span class="activityPrice">${
-                                    event.evt_price ? event.evt_price : "Gratis"
+                                    event.evt_price
+                                        ? `$${event.evt_price}`
+                                        : "Gratis"
                                 }</span>
                                 ${buttonHTML}
                             </div>
@@ -191,9 +208,172 @@ document.addEventListener("DOMContentLoaded", function () {
             // Agregar el div anterior al contenedor de actividades
             activitiesContainer.appendChild(eventCard);
         });
-
     }
 
+    // Función para obtener todas las categorías
+    function categoryFilter(
+        categories,
+        events,
+        category_event,
+        locations,
+        participants
+    ) {
+        // Obtener el menu desplegable de categorías
+        const categoriesMenuContainer =
+            document.getElementById("dropdown-menu");
+        // Obtener el botón de filtro de categorias
+        const categoriesFilterButton =
+            document.querySelector(".categoryFilter");
+        // Obtener el contenedor del contenido del botón de filtro
+        const filterButtonContent = document.getElementById(
+            "filterButtonContent"
+        );
+
+        // Validar que los elementos HTML existen antes de continuar.
+        if (
+            !categoriesMenuContainer ||
+            !categoriesFilterButton ||
+            !filterButtonContent
+        ) {
+            console.error(
+                "Error: No se encontraron los elementos HTML necesarios (dropdown-menu, categoryFilter o categoryFilterContent)."
+            );
+            return;
+        }
+
+        // Verificar si hay categorías disponibles
+        if (categories.length === 0) {
+            categoriesMenuContainer.innerHTML =
+                "<label>No hay categorías disponibles</label>";
+            // Deshabilitar el botón si no hay categorías para filtrar
+            categoriesFilterButton.disabled = true;
+            return;
+        }
+
+        // Limpiar el contenido actual del contenedor antes de agregar las categorías y se agrega la opción de todas las categorías
+        categoriesMenuContainer.innerHTML =
+            "<div data-value='allCategories'>Todas las categorias</div>";
+
+        // Agregar evento de clic para alternar la visibilidad del menú desplegable
+        categoriesFilterButton.addEventListener("click", function (event) {
+            event.stopPropagation(); // Evitar la propagación del evento
+            categoriesMenuContainer.classList.toggle("show");
+        });
+
+        // Event listener para cerrar el menú si se hace clic fuera de él
+        document.addEventListener("click", function (event) {
+            if (
+                !categoriesFilterButton.contains(event.target) &&
+                !categoriesMenuContainer.contains(event.target) && // Asegurarse de que el clic no fue dentro del menú
+                categoriesMenuContainer.classList.contains("show")
+            ) {
+                categoriesMenuContainer.classList.remove("show");
+            }
+        });
+
+        // Iterar sobre las categorías y crear opciones de menú desplegable
+        categories.forEach((category) => {
+            // Crear un elemento de opción para cada categoría
+            const optionCategory = document.createElement("div");
+            // Agregar contenido al elemento de opción
+            optionCategory.dataset.value = category.cat_id;
+            optionCategory.textContent = category.cat_name;
+            // Agregar el elemento de opción al contenedor del menú desplegable
+            categoriesMenuContainer.appendChild(optionCategory);
+        });
+
+        // Agregar evento de clic para cada opción de categoría
+        categoriesMenuContainer.addEventListener("click", function (event) {
+            // Verificar si el elemento clickeado es una opción de categoría comprobando que su etiqueta HTML sea DIV
+            if (event.target.tagName === "DIV") {
+                // Obtener el valor de la categoría seleccionada
+                const selectedCategoryId = event.target.dataset.value;
+                // Actualizar el texto del botón con la categoría seleccionada
+                filterButtonContent.textContent = event.target.textContent;
+                // Cerrar el menú desplegable
+                categoriesMenuContainer.classList.remove("show");
+                // Llamar a la función para filtrar eventos por categoría
+                filterEventsByCategory(
+                    selectedCategoryId,
+                    events,
+                    categories,
+                    category_event,
+                    locations,
+                    participants
+                );
+            }
+        });
+
+        // Nueva función para filtrar eventos por categoría
+        function filterEventsByCategory(
+            selectedCategoryId,
+            allEvents,
+            categories,
+            category_event,
+            locations,
+            participants
+        ) {
+            // Crear un array para almacenar los eventos filtrados
+            let filteredEvents = [];
+
+            // Si se selecciona "Todas las categorías", mostrar todos los eventos
+            if (selectedCategoryId === "allCategories") {
+                filteredEvents = allEvents; // Mostrar todos los eventos
+            } else {
+                // Convertir el valor de selectedCategoryId a un número entero
+                selectedCategoryId = parseInt(selectedCategoryId, 10);
+                // Filtrar las relaciones category_event por la categoría seleccionada
+                const eventIdsInCategory = category_event
+                    .filter((rel) => rel.FK_cat === selectedCategoryId)
+                    .map((rel) => rel.FK_evt);
+
+                // Filtrar los eventos que tienen los IDs encontrados
+                filteredEvents = allEvents.filter((event) =>
+                    eventIdsInCategory.includes(event.evt_id)
+                );
+            }
+
+            countTotalEvents(filteredEvents); // Actualizar el conteo de actividades
+
+            // Volver a renderizar los eventos filtrados
+            renderEventos(
+                filteredEvents,
+                categories,
+                category_event,
+                locations,
+                participants
+            );
+        }
+    }
+
+    // Función para filtrar eventos por nombre
+    function filterEventsByName(
+        events,
+        participants,
+        locations,
+        categories,
+        category_event
+    ) {
+        // Obtener el contenedor del input de búsqueda
+        const searchContainer = document.getElementById("search");
+        // Obtener el valor del input de búsqueda
+        const searchData = document.getElementById("search").value.toLowerCase();
+
+        // Verificar si el input de búsqueda está vacío
+        if (searchData === "") {
+            // Si el input de búsqueda está vacío, mostrar todos los eventos
+            renderEventos(
+                events,
+                categories,
+                category_event,
+                locations,
+                participants
+            );
+        }
+
+        
+
+    }
 });
 
 // Función para volver a la página principal
