63552330cb093a6f008f6147baa9f91c4f407635
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-08-17 18:59:41 -0500

    Refactorización (calendario): Reestructurar la página del calendario e implementar la funcionalidad de encabezado de usuario.
    - Eliminar los controladores onclick en línea y reemplazarlos con JavaScript modular.
    - Implementar la funcionalidad de visualización del encabezado de usuario mediante módulos importados.
    - Añadir la obtención y el procesamiento de datos JSON para la información del usuario.
    - Actualizar la lógica de navegación para usar BASE_URL y lograr un manejo de rutas consistente.

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,33 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const activities = document.querySelectorAll('.activity');
+import showUserHeader from "./functions/showUserHeader.js";
+import getLocalUsers from "./functions/getLocalUsers.js";
 
-    activities.forEach(activity => {
-        activity.addEventListener('click', function() {
-            window.location.href = "../templates/activity.html";
-        });
-    });
+/* --------------------------- VARIABLES GLOBALES ---------------------------- */
 
-    const activityCount = activities.length;
-    const countElement = document.querySelector('.activityCount');
-    const calendarSubtitle = document.querySelector('.calendarSubtitle');
-    if (countElement && calendarSubtitle) {
-        countElement.textContent = `${activityCount} actividades`;
-        calendarSubtitle.textContent = `${activityCount} actividades programadas para 2025`;
-    }
+// Variable para la URL base del proyecto
+const BASE_URL = window.location.hostname.includes("github.io")
+    ? "/cultura-de-barrio" // Ruta en GitHub Pages
+    : ""; // Ruta en entorno local
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch(`${BASE_URL}/resources/data/data.json`)
+        .then((response) => {
+            if (!response.ok)
+                throw new Error("No se pudo cargar el archivo JSON");
+            return response.json();
+        })
+        // Procesa los datos JSON
+        .then((data) => {
+            // Accede a los datos JSON y los guarda en variables
+            const users = data.USERS;
+            const categories = data.CATEGORIES;
+            const category_event = data.CATEGORY_EVENT;
+            const events = data.EVENTS;
+            const locations = data.LOCATIONS;
+            const participants = data.PARTICIPANTS;
+
+            // Llamar a la función de mostrar información del usuario logueado
+            showUserHeader(
+                (users = unirDatosJsonLocal(users, getLocalUsers())),
+                events
+            );
+        })
+        // Maneja errores
+        .catch((error) => console.error("Error:", error));
 });
-
-function back_to_home() {
-    window.location.href = "../index.html"
-}
-
-function go_to_activities( ) {
-    window.location.href = "../templates/activities.html"
-}
-
-function go_to_statistics( ) {
-    window.location.href = "../templates/statistics.html"
-}
-
-function go_to_login( ) {
-    window.location.href = "../templates/login.html"
-}
