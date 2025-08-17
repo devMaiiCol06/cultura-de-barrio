5f39fb87fde8bb1bc203244d938a34d423273abd
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-08-17 13:38:54 -0500

    Corrección: Actualizar la clave de almacenamiento local para que coincida con la lógica de registro.
    Se cambió la clave de "users" a "usersRegistered" para mantener la coherencia con la función de registro, que utiliza la misma clave.

--- scripts/functions/getLocalUsers.js
+++ scripts/functions/getLocalUsers.js
@@ -5,6 +5,6 @@
 // Función para obtener usuarios registrados en el localStorage
 export default function getLocalUsers() {
     // Obtener usuarios registrados en el localStorage
-    const users = JSON.parse(localStorage.getItem("users"));
+    const users = JSON.parse(localStorage.getItem("usersRegistered"));
     return users;
 }
