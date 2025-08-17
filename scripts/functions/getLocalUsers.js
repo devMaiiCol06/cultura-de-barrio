4baedda4733e9487ab3b0b19c6577913144087de
devMaiiCol06 <maicolmoreno1218@gmail.com>
2025-08-17 14:55:56 -0500

    Corrección (getLocalUsers): Gestionar usuarios faltantes y decodificar datos almacenados.
    Añadir gestión de errores para usuarios faltantes en localStorage e implementar la decodificación de datos de usuarios almacenados. Estos cambios garantizan la correcta recuperación y procesamiento de datos cuando los usuarios se almacenan en formato base64.

--- scripts/functions/getLocalUsers.js
+++ scripts/functions/getLocalUsers.js
@@ -6,5 +6,26 @@
 export default function getLocalUsers() {
     // Obtener usuarios registrados en el localStorage
     const users = JSON.parse(localStorage.getItem("usersRegistered"));
-    return users;
+    // Verificar si hay usuarios registrados
+    if (!users) {
+        // Si no hay usuarios registrados, mostrar mensaje de error
+        console.error("No hay usuarios registrados en el localStorage");
+        // Salir de la función
+        return;
+    }
+
+    // Decodificar los usuarios
+    const decodedUsers = users.map((user) => {
+        return {
+            user_id: parseInt(atob(user.user_id)),
+            user_name: atob(user.user_name),
+            user_lastname: atob(user.user_lastname),
+            user_mail: atob(user.user_mail),
+            user_password: atob(user.user_password),
+            user_birth: parseInt(atob(user.user_birth)),
+        };
+    });
+
+    // Devolver los usuarios decodificados
+    return decodedUsers;
 }
