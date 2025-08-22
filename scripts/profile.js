7e254ee816224a66ba73df43f67ae131076c7ed1
Jhoan Guerrero <jhoan131299.guerrero@gmail.com>
2025-08-22 18:30:38 -0500

    Añadir funcionalidad de modal para notificaciones y mejorar estilos en el perfil

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,16 +1,49 @@
-// LLamar el nav
-const tabs = document.querySelectorAll('.tab_btn');
-//LLamar al contigo que deas mostrar
-const contents = document.querySelectorAll('.container-box');
+document.addEventListener("DOMContentLoaded", () => {
+    const modalActivation = document.querySelector(".notification-btn");
+    const modalContainer = document.querySelector(".container-modal");
+    const modalContent = document.querySelector(".content-modal")
+    const closeModal = document.querySelector(".btn-cerrar-modal")
 
-tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        // Quitar clase "active" de todos
-        tabs.forEach(t => t.classList.remove('active'));
-        contents.forEach(c => c.classList.remove('active'));
+    modalActivation.addEventListener("click", (event) => {
+        event.stopPropagation();
+        modalContainer.classList.add("view");
+        modalContainer.classList.remove("hidden");
+    });
 
-        // Activar solo el clicado
-        tab.classList.add('active');
-        contents[index].classList.add('active');
+    // Ocultar el modal si se da clic fuera de él
+    document.addEventListener("click", (event) => {
+        if (
+            modalContainer.classList.contains("view") &&
+            !modalContent.contains(event.target) &&
+            !modalActivation.contains(event.target)
+        ) {
+            modalContainer.classList.remove("view");
+            modalContainer.classList.add("hidden");
+        }
+    });
+
+    closeModal.addEventListener('click', (event) => {
+        event.stopPropagation();
+        modalContainer.classList.remove("view");
+        modalContainer.classList.add("hidden");
+    }) 
+
+    // LLamar el nav
+    const tabs = document.querySelectorAll(".tab_btn");
+    //LLamar al contigo que deas mostrar
+    const contents = document.querySelectorAll(".container-box");
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+            // Quitar clase "active" de todos
+            tabs.forEach((t) => t.classList.remove("active"));
+            contents.forEach((c) => c.classList.remove("active"));
+
+            // Activar solo el clicado
+            tab.classList.add("active");
+            contents[index].classList.add("active");
+        });
     });
 });
+
+
